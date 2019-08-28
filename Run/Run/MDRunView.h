1e3dfd7da04a14e1085b08dc35fbec100b887e75
李永杰 <yj.li@muheda.com>
2019-08-28 19:08:50 +0800

    添加view

---
+++ Run/Run/MDRunView.h
@@ -0,0 +1,17 @@
+//
+//  MDRunView.h
+//  Run
+//
+//  Created by 李永杰 on 2019/8/28.
+//  Copyright © 2019 muheda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MDRunView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
